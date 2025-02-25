4fd1d0b6e29ba4ecb3b341e9a1cf0455ad03f430
danielaniceto <danielgustavots@hotmail.com>
2025-02-25 20:02:02 -0300

    Subindo atualizações

--- backend/controllers/login_controller.py
+++ backend/controllers/login_controller.py
@@ -2,21 +2,22 @@ import logging
 from schemas.login_schema import RequestUserModel
 from database.mongo import DataBaseConnection
 
-SERVICE_DB_CONECTION = DataBaseConnection()["users"]
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler("checklist_controller.log"), logging.StreamHandler()]
+)
+
+SERVICE_DB_CONECTION = DataBaseConnection()["checklis"]
 
 class LoginValidation:
     def __init__(self):
-        logging.basicConfig(
-            level=logging.INFO,
-            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            handlers=[logging.FileHandler("checklist_controller.log"), logging.StreamHandler()]
-
-            if SERVICE_DB_CONECTION is None:
+    
+        if SERVICE_DB_CONECTION is None:
             logging.critical("A conexão com banco de dados falhou, RECORDER_CONTROLLER")
             raise ValueError("A conexão com banco falhou") 
-)
-        
+       
     def validate(data: RequestUserModel):
-        if self.user.username == 'admin' and self.user.password == 'admin':
+        if data.username == 'email' and data.password == 'password':
             return True
         return False
